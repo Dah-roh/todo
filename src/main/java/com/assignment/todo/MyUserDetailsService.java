708514c82dae1943d990b829ff145eb337a0fdc9
dizsart <darogadibia@gmail.com>
2020-02-23 02:41:50 +0100

    made some changes to the userprincipal class in the file structure

--- src/main/java/com/assignment/todo/MyUserDetailsService.java
+++ src/main/java/com/assignment/todo/MyUserDetailsService.java
@@ -1,13 +1,22 @@
 package com.assignment.todo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.nio.file.attribute.UserPrincipal;
+
 public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository repositoryObject;
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-
-        return null;
+        User user = repositoryObject.findByUsername();
+        if(user == null){
+            throw new UsernameNotFoundException("This user was not found");
+        }
+        return UserPrincipal(user);
     }
 }
--- src/main/java/com/assignment/todo/UserRepository.java
+++ src/main/java/com/assignment/todo/UserRepository.java
@@ -1,4 +1,7 @@
 package com.assignment.todo;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
