0c2777a025039a133598bfaffb86f8aa4c2cb507
dizsart <darogadibia@gmail.com>
2020-02-23 02:23:56 +0100

    implemented the methods from the user details service interface

--- src/main/java/com/assignment/todo/MyUserDetailsService.java
+++ src/main/java/com/assignment/todo/MyUserDetailsService.java
@@ -1,4 +1,13 @@
 package com.assignment.todo;
 
-public class MyUserDetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+
+        return null;
+    }
 }
